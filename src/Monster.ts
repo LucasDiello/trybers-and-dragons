7ff048ad1dcfea934997e91d8b49e40b72e9a76b
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-10-26 17:04:50 -0300

    req 9

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints: number = 85,
+    private _strength: number = 63,
+  ) {
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+  
+  receiveDamage(attackPoints: number) : number {
+    if (this._lifePoints < 0) {
+      this._lifePoints = -1;
+    }
+    this._lifePoints -= attackPoints;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);    
+  }
+}
+
+export default Monster;     
