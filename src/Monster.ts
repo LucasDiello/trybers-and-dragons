c51923860ec1ec522e0e8c49c7d2777ad98b298b
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-11-18 15:32:54 -0300

    fix error

--- src/Monster.ts
+++ src/Monster.ts
@@ -2,17 +2,13 @@ import { SimpleFighter } from './Fighter';
 
 class Monster implements SimpleFighter {
   constructor(
-    private _lifePoints: number = 85,
+    public _lifePoints: number = 85,
     private _strength: number = 63,
   ) {
   }
 
   get lifePoints(): number {
     return this._lifePoints;
-  }
-
-  set lifePoints(lifePoints: number) {
-    this._lifePoints = lifePoints;
   }
 
   get strength(): number {
